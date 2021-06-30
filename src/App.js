a98f950792d87ad1bc6208d34432fbdedcf69142
Ali Eren Yogurtcu <aeren.yogurtcu@gmail.com>
2021-06-30 14:17:04 +0300

    Removed horizontal scroll, used fluid container

--- src/App.js
+++ src/App.js
@@ -7,38 +7,37 @@ import Answers from './components/answers';
 import { Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
   //Link
 } from "react-router-dom";
 
+import Container from 'react-bootstrap/Container';
+
 const App = () => {
-  
-    return (
-      <Router>
-      <div>
-        <Row>
-          <Header />
-        </Row>
-        <Row className="bodyLayout">
-          <Col sm={3} className="sidenav mobil">
-            <Sidebar />
-          </Col>
-          <Col className="mainLayout">
-           
-              <Switch>
-                <Route exact path="/Main"> 
-                  <Main />
-                </Route>
-                <Route path="/questionEntry" component={QuestionEntry} />
-                <Route path="/answers" component={Answers} />
-              </Switch>
-            
-          </Col>
-        </Row>
-      </div>
-      </Router>
+
+  return (
+      <Container fluid>
+      <Row>
+        <Header />
+      </Row>
+      <Row className="bodyLayout">
+        <Col sm={3} className="sidenav mobil">
+          <Sidebar />
+        </Col>
+        <Col className="mainLayout">
+
+          <Switch>
+            <Route exact path="/Main">
+              <Main />
+            </Route>
+            <Route path="/questionEntry" component={QuestionEntry} />
+            <Route path="/answers" component={Answers} />
+          </Switch>
+
+        </Col>
+      </Row>
+    </Container>
     );
   
 }
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import store, { persistor } from "./store/"
 import { BrowserRouter } from "react-router-dom";
 import { positions, Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
+import { BrowserRouter as Router} from 'react-router-dom';
 const options = {
   timeout: 5000,
   position: positions.BOTTOM_CENTER
@@ -18,7 +19,9 @@ ReactDOM.render(
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <AlertProvider template={AlertTemplate} {...options}>
-          <App />
+          <Router>
+            <App />
+          </Router>
         </AlertProvider>
       </BrowserRouter>
     </PersistGate>
